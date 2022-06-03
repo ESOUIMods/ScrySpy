1687bc95965df3242f2c06218fcaebab079f608f
Sharlikran <danielhmpdx16@gmail.com>
2022-06-03 10:21:24 -0700

    Version: 1.38

--- Language/es.lua
+++ Language/es.lua
@@ -1,23 +1,21 @@
---[[
 local strings = {
-    mod_title                           = "ScrySpy",
-    map_pin_texture_text                = "Select map pin icons",
-    map_pin_texture_desc                = "Select map pin icons.",
-    digsite_texture_text                = "Select 3D-Pin Digsite icons",
-    digsite_texture_desc                = "Select 3D-Pin Digsite icons.",
-    pin_size                            = "Pin size",
-    pin_size_desc                       = "Set the size of the map pins.",
-    pin_layer                           = "Pin layer",
-    pin_layer_desc                      = "Set the layer of the map pins so they overlap others at the same location.",
-    show_digsites_on_compas             = "Show Digsites on the compass",
-    show_digsites_on_compas_desc        = "Show/hide icons for Digsites on the compass.",
-    compass_max_dist                    = "Max pin distance",
-    compass_max_dist_desc               = "The maximum distance for pins to appear on the compass.",
-    spike_pincolor                      = "Color for lower 3D pin",
-    spike_pincolor_desc                 = "The color of the lower section of the 3D pin.",
+  mod_title = "ScrySpy",
+  map_pin_texture_text = "Ícono en el mapa",
+  map_pin_texture_desc = "Define el ícono del marcador que aparecerá en el mapa.",
+  digsite_texture_text = "Marcador de yacimiento 3D",
+  digsite_texture_desc = "Define el marcador 3D que aparecerá en los yacimientos.",
+  pin_size = "Tamaño del marcador",
+  pin_size_desc = "Selecciona el tamaño de los íconos en el mapa.",
+  pin_layer = "Nivel del marcador",
+  pin_layer_desc = "Define el nivel de los marcadores en el mapa para que aparezcan sobre o por debajo de otros marcadores en la misma ubicación.",
+  show_digsites_on_compas = "Mostrar yacimientos en la brújula",
+  show_digsites_on_compas_desc = "Muestra u oculta los marcadores de los yacimientos en la brújula.",
+  compass_max_dist = "Distancia máxima del marcador",
+  compass_max_dist_desc = "La distancia máxima en el que los marcadores aparecerán en la brújula",
+  spike_pincolor = "Color inferior de marcador 3D",
+  spike_pincolor_desc = "El color de la sección inferior del marcador 3D.",
 }
 
 for stringId, stringValue in pairs(strings) do
-    SafeAddString(_G[stringId], stringValue, 1)
+  SafeAddString(_G[stringId], stringValue, 1)
 end
-]]--
